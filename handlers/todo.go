205e10582ca9b22518179c6f1c9966c9e803ea14
togo-mentor <uint2435@gmail.com>
2023-10-15 07:41:06 +0900

    feat: routingの定義

---
+++ handlers/todo.go
@@ -0,0 +1,92 @@
+package handler
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/mholt/binding"
+	"github.com/togo-mentor/go-practice-app/usecases"
+	"github.com/togo-mentor/go-practice-app/db"
+)
+
+type TodoHandler struct {}
+
+func NewTodoHandler() *TodoHandler {
+	return &TodoHandler{}
+}
+
+// 失敗時のレスポンスの設定
+func setErrorResponse(w http.ResponseWriter, status int) {
+	// レスポンスのヘッダ設定
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+	// 引数のステータス設定
+	w.WriteHeader(status)
+}
+
+// 成功時のレスポンスの設定
+func setSuccessResponse(w http.ResponseWriter, res []byte) {
+	// レスポンスの内容をjsonに変換
+	w.Write(res)
+}
+
+// TODO作成API
+func (h *TodoHandler) Create(w http.ResponseWriter, r *http.Request) {
+	var err error
+	var berr binding.Errors
+	var response usecase.CreateTodoOutput
+	var res []byte
+
+	// request -> CreateTodoInput型に変換
+	var request usecase.CreateTodoInput
+	berr = binding.Bind(r, &request)
+	if berr != nil {
+		log.Println(berr)
+		setErrorResponse(w, http.StatusInternalServerError)
+		return
+	}
+
+	ctx := context.Background()
+
+	// dbのコネクション設定
+	conn, err := db.GetDBconnection()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	// コネクションのクローズ
+	defer func() {
+		conn.Close()
+	}()
+
+	// トランザクションの開始
+	tx, err := conn.BeginTx(ctx, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	// トランザクションのコミット or ロールバック
+	defer func() {
+		if err != nil {
+			tx.Rollback()
+			return
+		}
+		tx.Commit()
+	}()
+
+	// usecaseの初期化
+	TodoUsecase := usecase.NewTodoUsecase(ctx, tx)
+	// ユーザー作成実施
+	response, err = TodoUsecase.Create(request)
+
+	// レスポンスをjsonに変換
+	res, err = json.Marshal(response)
+	if err != nil {
+		log.Println(err)
+		setErrorResponse(w, http.StatusInternalServerError)
+		return
+	}
+	setSuccessResponse(w, res)
+}
