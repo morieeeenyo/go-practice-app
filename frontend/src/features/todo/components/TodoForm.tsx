226ad1624f74ecd30b8729544de6970d2c0fcbb0
togo-mentor <uint2435@gmail.com>
2023-11-23 08:38:46 +0900

    feat: TodoFormでフォームバリデーションを導入する

--- frontend/src/features/todo/components/TodoForm.tsx
+++ frontend/src/features/todo/components/TodoForm.tsx
@@ -1,23 +1,58 @@
 import React from "react"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+const validationSchema = z.object({
+  title: z.string().refine((value) => Boolean(value.trim().length), {
+    message: "タイトルを入力してください",
+  }),
+})
+
+type FormValues = z.infer<typeof validationSchema>
 
 export const TodoForm = () => {
+  const form = useForm<FormValues>({
+    mode: "onChange",
+    resolver: zodResolver(validationSchema),
+  })
+
+  const {
+    register,
+    formState: { errors, isValid },
+  } = form
   return (
     <div
       style={{
         display: "flex",
-        alignItems: "center",
+        alignItems: "flex-start",
         padding: "0 0 1rem",
       }}
     >
-      <input
-        type="text"
-        placeholder="TODOのタイトルを入力してください"
-        style={{
-          marginRight: "2rem",
-          minWidth: "20rem",
-        }}
-      />
-      <button>登録</button>
+      <div>
+        <input
+          {...register("title")}
+          name="title"
+          type="text"
+          placeholder="TODOのタイトルを入力してください"
+          style={{
+            marginRight: "2rem",
+            minWidth: "20rem",
+            display: "block",
+          }}
+        />
+        {errors.title && (
+          <span
+            style={{
+              color: "red",
+              fontSize: "0.75rem",
+            }}
+          >
+            {errors.title?.message}
+          </span>
+        )}
+      </div>
+      <button disabled={!isValid}>登録</button>
     </div>
   )
 }
