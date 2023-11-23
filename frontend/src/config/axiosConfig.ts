23bd9884095444ab3672ca045cdbcd43130adbfe
togo-mentor <uint2435@gmail.com>
2023-11-23 11:34:26 +0900

    feat: formを送信する関数の定義

--- frontend/src/config/axiosConfig.ts
+++ frontend/src/config/axiosConfig.ts
@@ -1,7 +1,19 @@
 import axios from "axios"
+import { Arguments } from "swr"
+
+type Args ={
+  arg: Arguments
+}
 
 const axiosDefault = axios.create({
   baseURL: "http://localhost:8080",
 })
 
-export const get = (path: string) => axiosDefault.get(path).then((res) => res.data)
+export const get = (path: string) =>
+  axiosDefault.get(path).then((res) => res.data)
+
+export const post = async (url: string, { arg }: Args) => {
+  const response = await axiosDefault.post(url, arg)
+
+  return response.data
+}
