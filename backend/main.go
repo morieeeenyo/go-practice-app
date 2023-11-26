3d839d7489f13fbdb1123e39e3b3ee28632bbbf5
togo-mentor <uint2435@gmail.com>
2023-11-26 12:34:58 +0900

    refactor: 初回起動時にcmdディレクトリのコマンドを実行せず、mainを直接動かす

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,35 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "github.com/togo-mentor/go-practice-app/cmd"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+
+	"github.com/togo-mentor/go-practice-app/router"
+)
 
 func main() {
-	cmd.Execute()
+	e := echo.New()
+	e = router.InitRouter(e)
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+    /* CORS(オリジン間リソース共有)の設定 */
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+        /* http://localhost:3000からの接続を許可する */
+		AllowOrigins: []string{"http://localhost:3000"},
+		AllowMethods: []string{
+			http.MethodGet,
+			http.MethodPut,
+			http.MethodPost,
+			http.MethodDelete,
+		},
+	}))
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
--- backend/router/router.go
+++ backend/router/router.go
@@ -1,17 +1,32 @@
 package router
 
 import (
-	"github.com/togo-mentor/go-practice-app/handlers"
-	"github.com/go-chi/chi"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+
+	handler "github.com/togo-mentor/go-practice-app/handlers"
+	"github.com/togo-mentor/go-practice-app/infra"
+	"github.com/togo-mentor/go-practice-app/infra/mysql"
+	service "github.com/togo-mentor/go-practice-app/services"
+	usecase "github.com/togo-mentor/go-practice-app/usecases"
 )
 
-func Get() *chi.Mux {
-	r := chi.NewRouter()
+func InitRouter(e *echo.Echo) *echo.Echo {
+	dbConnection := infra.InitDBConnection()
+	todoRepository := mysql.NewTodoRepository(dbConnection)
+	todoService := service.NewTodoService(todoRepository)
+	todoUsecase := usecase.NewTodoUsecase(todoService)
+	g := e.Group("/todos")
+	todoHandler := handler.NewTodoHandler(todoUsecase)
+	{
+		g.POST("", todoHandler.CreateTodo())
+		g.GET("", todoHandler.FindyAllTodos())
+	}
 
-	todoHandler := handler.NewTodoHandler()
-	r.Route("/todos", func(r chi.Router) {
-		r.Post("/", todoHandler.Create)
-		r.Get("/", todoHandler.Get)
-	})
-	return r
+	return e
+}
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
 }
